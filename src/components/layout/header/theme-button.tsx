6391c22de9c0e6a81fc0bbc035dd87766897c06c
chaehyeon <kch971109@naver.com>
2026-01-01 22:10:25 +0900

    ✨  feat: 테마(다크, 라이트, 시스템) 변경 기능 구현

---
+++ src/components/layout/header/theme-button.tsx
@@ -0,0 +1,48 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import type { Theme } from "@/types";
+import { PopoverClose } from "@radix-ui/react-popover";
+import { SunIcon } from "lucide-react";
+
+const THEMES: Theme[] = ["system", "dark", "light"];
+
+const onChangeTheme = (theme: Theme) => {
+  const htmlTag = document.documentElement;
+  htmlTag.classList.remove("light", "dark");
+  if (theme === "system") {
+    const isDarkTheme = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+
+    htmlTag.classList.add(isDarkTheme ? "dark" : "light");
+  } else {
+    htmlTag.classList.add(theme);
+  }
+};
+
+export default function ThemeButton() {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <div className="hover:bg-muted cursor-pointer rounded-full p-2">
+          <SunIcon />
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="w-35 p-0">
+        {THEMES.map((theme) => (
+          <PopoverClose key={`theme-button-${theme}`} asChild>
+            <div
+              onClick={() => onChangeTheme(theme)}
+              className="hover:bg-muted cursor-pointer p-3"
+            >
+              {theme}
+            </div>
+          </PopoverClose>
+        ))}
+      </PopoverContent>
+    </Popover>
+  );
+}
