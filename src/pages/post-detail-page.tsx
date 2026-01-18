ba5796fba81b945b04cab2b3cfea904c7fd7630f
chaehyeon <kch971109@naver.com>
2026-01-18 16:31:11 +0900

    feat: 상세 페이지 댓글 수 표시 추가

--- src/pages/post-detail-page.tsx
+++ src/pages/post-detail-page.tsx
@@ -1,11 +1,17 @@
 import CommentEditor from "@/components/comment/comment-editor";
 import CommentList from "@/components/comment/comment-list";
 import PostItem from "@/components/post/post-item";
+import { usePostByIdData } from "@/hooks/queries/use-post-by-id-data";
 import { Navigate, useParams } from "react-router";
 
 export default function PostDetailPage() {
   const params = useParams();
   const postId = params.postId;
+
+  const { data: post } = usePostByIdData({
+    postId: Number(postId),
+    type: "DETAIL",
+  });
 
   if (!postId) {
     return <Navigate to={"/"} />;
@@ -14,7 +20,7 @@ export default function PostDetailPage() {
   return (
     <div className="flex flex-col gap-5">
       <PostItem postId={Number(postId)} type={"DETAIL"} />
-      <div className="text-cl font-bold"> 댓글 </div>
+      <div className="text-cl font-bold">댓글 {post?.comment_count ?? 0}</div>
       <CommentEditor type="CREATE" postId={Number(postId)} />
       <CommentList postId={Number(postId)} />
     </div>
